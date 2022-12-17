c9432494657727aa497371fba69e886f0321f5a2
Nicolai Bondarenco <nicbondarenco@gmail.com>
2022-12-17 14:19:03 +0200

    fix_data

--- src/App/App.js
+++ src/App/App.js
@@ -4,51 +4,88 @@ import './App.css'
 
 const App = () => {
   const stateRef = useRef({})
+  const [updateTime, setUpdateTime] = useState('')
   const [fromMenu, setFromMenu] = useState(false)
   const [toMenu, setToMenu] = useState(false)
   const [fromValue, setFromValue] = useState('USD')
   const [toValue, setToValue] = useState('UAH')
-  const [fromCurrency, setFromCurrency] = useState(0)
+  const [fromCurrency, setFromCurrency] = useState(1)
   const [toCurrency, setToCurrency] = useState(0)
+  const [fromError, setFromError] = useState(false)
+  const [toError, setToError] = useState(false)
+  console.log(updateTime.last_updated_at)
 
-  const onChangeFromCurrency = (v) => {
-    const res =
-      (stateRef.current[toValue].value / stateRef.current[fromValue].value) * v
-    setFromCurrency(v)
-    setToCurrency(res.toFixed(3))
-  }
+  const f = updateTime.last_updated_at.split('T')
+  console.log(f)
 
   useEffect(() => {
     fetch('http://localhost:3000/data.json')
       .then((res) => res.json())
       .then((body) => {
-        setFromCurrency(1)
         stateRef.current = body.data
-        onChangeFromCurrency(1)
+        setUpdateTime(body.meta)
+        onChangeFromCurrency(fromCurrency)
       })
       .catch((err) => console.log(err))
-  }, [])
+  }, [fromValue, toValue])
+
+  const onChangeFromValue = (cur) => {
+    setFromValue(cur)
+  }
+
+  const onChangeToValue = (cur) => {
+    setToValue(cur)
+  }
+
+  const onChangeFromCurrency = (v) => {
+    if (v < 0) {
+      return setFromError(true)
+    } else if (v >= 0) {
+      setFromError(false)
+      const res =
+        (stateRef.current[toValue].value / stateRef.current[fromValue].value) *
+        v
+      setToCurrency(res.toFixed(3))
+      setFromCurrency(v)
+    }
+  }
+
+  const onChangeToCurrency = (v) => {
+    if (v < 0) {
+      return setToError(true)
+    } else if (v >= 0) {
+      setToError(false)
+      const res =
+        (stateRef.current[fromValue].value / stateRef.current[toValue].value) *
+        v
+      setFromCurrency(res.toFixed(3))
+      setToCurrency(v)
+    }
+  }
 
   return (
     <div className="app">
       <header className="header">
         <Block
+          error={fromError}
           currency={fromCurrency}
-          changeCurrency={(v) => onChangeFromCurrency(v)}
+          changeCurrency={onChangeFromCurrency}
           value={fromValue}
           menu={fromMenu}
           setMenu={() => setFromMenu((prev) => !prev)}
-          changeValue={(el) => setFromValue(el)}
+          changeValue={onChangeFromValue}
         />
         <Block
+          error={toError}
           currency={toCurrency}
-          changeCurrency={(v) => setToCurrency(v)}
+          changeCurrency={onChangeToCurrency}
           value={toValue}
           menu={toMenu}
           setMenu={() => setToMenu((prev) => !prev)}
-          changeValue={(el) => setToValue(el)}
+          changeValue={onChangeToValue}
         />
       </header>
+      <div className="app__time"></div>
     </div>
   )
 }
--- src/Block/index.js
+++ src/Block/index.js
@@ -3,7 +3,15 @@ import Menu from '../Menu'
 import './index.css'
 
 const Block = (props) => {
-  const { value, changeValue, menu, setMenu, currency, changeCurrency } = props
+  const {
+    value,
+    changeValue,
+    menu,
+    setMenu,
+    currency,
+    changeCurrency,
+    error,
+  } = props
 
   return (
     <div className="block">
@@ -13,11 +21,17 @@ const Block = (props) => {
           value={currency || ''}
           onChange={(e) => changeCurrency(e.target.value)}
           type="number"
+          placeholder="0"
         />
         <button className="block__select" onClick={setMenu}>
           {value}
         </button>
       </div>
+      {error && (
+        <span style={{ color: 'red' }}>
+          Значение не может быть меньше нуля!
+        </span>
+      )}
       {menu && <Menu setMenu={setMenu} changeValue={changeValue} />}
     </div>
   )
